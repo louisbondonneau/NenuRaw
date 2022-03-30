94c15b706047fa9bc1e0db6d726aca2aee587397
Louis Bondonneau <louis@nanplb.obs-nancay.fr>
2022-03-30 17:22:49 +0200

    rm demo/.DS_Store

---
+++ beta_dir/test_rawtf.py
@@ -0,0 +1,26 @@
+from NenuRaw import dynspec_utils
+from NenuRaw import wav_utils
+
+if __name__ == "__main__":
+    # wavfile (GUPPI or RAWTF format)
+    files = ['/databf2/nenufar-tf/ES00/2022/02/20220228_154600_20220228_155400_B0809+74_TRACKING/B0809+74_TRACKING_20220228_154637_2.raw']
+
+    # initialisation of the Dynspec object containing the methodes
+    my_spectra = dynspec_utils.Dynspec(files,
+                                       verbose=False,
+                                       freq_start=0,
+                                       freq_end=99,
+                                       start=10,  # start time in sec
+                                       end=10.4  # stop time in sec
+                                       )
+
+    # force dm to 0 pc cm-3 or will use the dm from the header in PSR obs
+    my_spectra.dm = 0
+
+    # execution of the processing method in the Fourier domain creating 64 sub-channels (~3 kHz channel)
+    # and conversion to total intensity with time integration of 100 ms
+    my_spectra.fourier_computation(fftlen=1, ds_ms=4 * 5.12e-6, pol="I")  # I, Q, U, L, V, XX, YY
+
+    # plot spectrum
+    # my_spectra.clean(threshold=10) # clean with threshold=10
+    my_spectra.plot_spectra()
